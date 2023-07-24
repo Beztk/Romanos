ebfc9881062e55a8640bc74cb0572cee5ccb3292
Santi <elguzman_16@hotmail.com>
2023-07-24 13:42:46 -0700

    feat: nuevos cambios

--- romanos.py
+++ romanos.py
@@ -1,8 +1,10 @@
 def convertir_a_romano(numero):
     if type(numero) != int:
         return f"Error: debes introducir un numero entero ({numero})"
-    elif type(numero) == int:
-        return "TODO: convertir a romano"
+
+    # validar el numero
+    if not (0 < numero < 4000):
+        return f"Error: el numero debe estar entre 1 y 3999 ({numero})"
 
 
 def comprobar_entero(numero):
@@ -18,3 +20,7 @@ print(convertir_a_romano(56))
 print(convertir_a_romano(56.1))
 print(convertir_a_romano([]))
 print(convertir_a_romano({}))
+print(convertir_a_romano(0))
+print(convertir_a_romano(4000))
+print(convertir_a_romano(1))
+print(convertir_a_romano(3999))
