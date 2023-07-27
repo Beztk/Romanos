6bd72be334e90d94b44d16b465ceeaaff1a93c88
Santi <elguzman_16@hotmail.com>
2023-07-26 22:46:22 -0700

    Copia hecha

--- romanos.py
+++ romanos.py
@@ -1,7 +1,4 @@
 """
-Escribir una función en PYTHON que reciba un número entre 0 y 3999
-y devuelva el número como una cadena en su representación de número romano.
-
 
 I  ---> 1
 V  ---> 5
@@ -18,18 +15,6 @@ M  ---> 1000
 |________________ M   ---- 1 * 10³
 
 1M 1C 3D 7U
-
-1137 / 1000 = 1 ----- diccionario: M
-1137 % 1000 = 137
-
-137 / 100 = 1 ------- diccionario: C
-137 % 100 = 37
-
-37 / 10 = 3 --------- diccionario: XXX
-37 % 10 = 7 
-
-7 / 1 = 7 ----------- diccionario: VII
-7 % 1 = 0
 """
 
 
@@ -41,69 +26,114 @@ def convertir_a_romano(numero):
     if not (0 < numero < 4000):
         return f"Error: el número debe estar entre 1 y 3999 ({numero})"
 
-    millares = ["", "M", "MM", "MMM"]
-    centenas = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-    decenas = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
-    unidades = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    conversores = [
+        ["", "M", "MM", "MMM"],
+        ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+        ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+        ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+    ]
 
-    # valores = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'),
-    #           (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'),
-    #           (5, 'V'), (4, 'IV'), (1, 'I')]
+    divisores = [1000, 100, 10, 1]
 
-    """
-    1000
+    resultado = ""
+    contador = 0
 
-    2354
+    for divisor in divisores:
+        cociente = (numero // divisor)
+        numero = numero % divisor
+        resultado = resultado + conversores[contador][cociente]
+        contador = contador + 1
 
-    2354 - 1000 = 1354 :::: M
-    1354 - 1000 =  354 :::: MM
-    3540 -  100 =  254 :::: MMC
-    254  -  100 =  154      MMCC
-    154  -  100 =   54      MMCCC
-    54   -   50 =    4      MMCCCD
-    4    -    4 =    0      MMCCCDIV
-
-    como resto = 0 salgo del bucle
-    """
-
-    # descomposición en millares, decenas, centenas y unidades
-    millar = numero // 1000
-    centena = (numero % 1000) // 100
-    decena = ((numero % 1000) % 100) // 10
-    unidad = (((numero % 1000) % 100) % 10) // 1
-
-    """
-    millar = numero // 1000
-    resto = numero % 1000
-
-    centena = resto // 100
-    resto = resto % 100
-    
-    decena = resto // 10
-    resto = resto % 10
-    
-    unidad = resto // 1
-    resto = resto % 1
-    """
-
-    # RETO: podemos simplificar el proceso en lugar de copiar/pegar las operaciones de división y módulo?
-
-    # mapear el cociente (diccionario)
-    # si hay resto, repetimos...
-
-    romano = millares[millar] + centenas[centena] + \
-        decenas[decena] + unidades[unidad]
-
-    return romano
+    return resultado
 
 
-print(convertir_a_romano(56.1))
-print(convertir_a_romano("lo que quiera"))
-print(convertir_a_romano([]))
-print(convertir_a_romano({}))
-print(convertir_a_romano(0))
-print(convertir_a_romano(4000))
+def romano_a_entero(romano):
 
-print(convertir_a_romano(56))
-print(convertir_a_romano(1))
-print(convertir_a_romano(3999))
+    digitos_romanos = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    romanos_repetidos = ['IIII', ' VVVV',
+                         'XXXX', 'LLLL', 'CCCC', 'DDDD', 'MMMM']
+
+    if not isinstance(romano, str):
+        return 'ERROR: tiene que ser un número romano en formato cadena de texto'
+
+    resultado = 0
+    anterior = 0
+
+    for letra in romano:
+        if letra not in digitos_romanos:
+            return f'ERROR: {letra} no es un dígito romano válido (I, V, X, L, C, D, M)'
+        actual = digitos_romanos[letra]
+
+        if valor in romanos_repetidos:
+            return f'ERROR: {valor} no es permitido'
+
+        if anterior < actual:
+            # comprobar que la resta es posible
+            # el orden de magnitud no es mayor de uno
+
+            if anterior > 0 and len(str(actual)) - len(str(anterior)) > 1:
+                return f"ERROR: resta no posible (ant: {anterior}, act: {actual})"
+
+            # deshacer la suma (que hemos hecho en la iteración anterior)
+            # DCI --- DC
+            resultado = resultado - anterior
+            # DC(IV) 600 + 4 = 600 + (5 - 1) = resultado + (actual - anterior)
+            # sumar el valor actual, pero restando el anterior
+            resultado = resultado + (actual - anterior)
+        else:
+            resultado = resultado + actual
+
+        anterior = actual
+
+    return resultado
+
+
+"""
+
+3 -- 56
+
+        10³10²10¹10⁰
+.  .  .  .  .  .  .
+         3  0  0  0  3*10³ --- 4
+            3  0  0  3*10² --- 3
+               3  0  3*10¹ --- 2
+                  3  3*10⁰ --- 1
+
+midamos la longitud del número
+
+I - 1           1*10⁰
+X - 10          1*10¹
+
+I - 1           1*10⁰
+C - 100         1*10²
+
+I   1*10⁰
+V   5*10⁰
+
+I   1*10⁰
+D   5*10²
+
+
+I -- 1
+D -- 500
+len("1") 1
+len("500") 3
+"""
+
+
+errores = ['A', '', 3, ['X', 'X', 'I']]
+pruebas = ['IIII', 'I', 'MCXXIII', 'VIII', 'LVI', 'IV',
+           'IX', 'XC', 'CM', 'IC', 'IM', 'XM', 'ID', 'VX']
+
+for valor in pruebas:
+    print(romano_a_entero(valor))
+# print(romano_a_entero('MCXCIV'))
